4e370a5bacf5f7f92644ac830e7e2c271918d632
Lisek <99961781+lisekv1@users.noreply.github.com>
2025-11-11 17:16:51 +0100

    Add files via upload

---
+++ moreYield.lua
@@ -0,0 +1,182 @@
+-- Author: westor
+-- Contact: westor7 @ Discord
+-- Github: https://github.com/westor7/FS25_moreYield
+--
+-- Copyright (c) 2025 westor
+
+moreYield = {}
+moreYield.settings = {}
+moreYield.initUI = false
+
+function moreYield.prerequisitesPresent(specializations)
+	return true
+end
+
+function moreYield:loadMap()
+	InGameMenu.onMenuOpened = Utils.appendedFunction(InGameMenu.onMenuOpened, moreYield.initUi)
+	
+	FSBaseMission.saveSavegame = Utils.appendedFunction(FSBaseMission.saveSavegame, moreYield.saveSettings)
+	
+	moreYield:loadSettings()
+
+	moreYield:Init()
+end
+
+function moreYield:mouseEvent(posX, posY, isDown, isUp, button)
+end
+
+function moreYield:keyEvent(unicode, sym, modifier, isDown)
+end
+
+function moreYield:update(dt)
+end
+
+function moreYield:draw(dt)
+end
+
+function moreYield:defSettings()
+	moreYield.settings.Multiplier = 2
+	moreYield.settings.OldMultiplier = 2
+end
+
+function moreYield:Init()
+	local updated = 0
+	local types = {
+			"WHEAT", 
+			"BARLEY",
+			"CANOLA",
+			"OAT", 
+			"MAIZE", 
+			"SUNFLOWER",
+			"SOYBEAN", 
+			"COTTON",
+			"SORGHUM",
+			"GRAPE", 
+			"OLIVE",
+			"POPLAR",
+			"GRASS",
+			"MEADOW",
+			"OILSEEDRADISH",
+			"RICE",
+			"RICELONGGRAIN",
+			"PEA",
+			"POTATO",
+			"CARROT",
+			"PARSNIP",
+			"BEETROOT", 
+			"SPINACH", 
+			"GREENBEAN", 
+			"SUGARBEET",
+			"SUGARCANE",
+			-- NF MARCH CUSTOM FRUITTYPES
+			"SPELT",
+			"RYE",
+			"TRITICALE",
+			"SUMMERWHEAT",
+			"SUMMERBARLEY",
+			"HEMP",
+			"LINSEED",
+			"ALFALFA",
+			"BEANS",
+			"PEAS"
+		}
+
+	for index, validFruit in pairs(g_fruitTypeManager.fruitTypes) do
+
+		for _, fruitTypeName in ipairs(types) do
+			local fruitType = g_fruitTypeManager:getFruitTypeByName(fruitTypeName)
+
+			if fruitType ~= nil and fruitType == validFruit then
+				local OldMultiplier = 0
+				
+				if moreYield.settings.OldMultiplier ~= moreYield.settings.Multiplier then OldMultiplier = moreYield.settings.OldMultiplier end
+			
+				local defLiters = g_fruitTypeManager.fruitTypes[index].defaultLiterPerSqm
+				local oldLiters = math.abs(tonumber(string.format("%.6f", g_fruitTypeManager.fruitTypes[index].literPerSqm)))
+				
+				if not defLiters then
+					g_fruitTypeManager.fruitTypes[index].defaultLiterPerSqm = oldLiters
+					
+					defLiters = oldLiters
+				end
+				
+				local newLiters = math.abs(tonumber(string.format("%.6f", defLiters * moreYield.settings.Multiplier)))
+				
+				g_fruitTypeManager.fruitTypes[index].literPerSqm = newLiters
+				
+				local supportWindrow = g_fruitTypeManager.fruitTypes[index].hasWindrow
+				local windrowName = g_fruitTypeManager.fruitTypes[index].windrowName
+				local defwindrowLiters = g_fruitTypeManager.fruitTypes[index].defaultwindrowLiterPerSqm
+				local windrowLiters = g_fruitTypeManager.fruitTypes[index].windrowLiterPerSqm
+				
+				if supportWindrow ~= nil and windrowLiters ~= nil then
+					local oldwindrowLiters = math.abs(tonumber(string.format("%.6f", windrowLiters)))
+					
+					if not defwindrowLiters then 
+						g_fruitTypeManager.fruitTypes[index].defaultwindrowLiterPerSqm = oldwindrowLiters
+						
+						defwindrowLiters = oldwindrowLiters	
+					end
+					
+					local newwindrowLiters = math.abs(tonumber(string.format("%.6f", defwindrowLiters + moreYield.settings.Multiplier)))
+					
+					g_fruitTypeManager.fruitTypes[index].windrowLiterPerSqm = newwindrowLiters
+				end
+				
+				updated = updated + 1
+			end
+		end
+	end
+end
+
+function moreYield:saveSettings()
+	local xmlPath = getUserProfileAppPath() .. "modSettings" .. "/" .. "moreYield.xml"
+	local xmlFile = createXMLFile("moreYield", xmlPath, "moreYield")
+	
+	setXMLFloat(xmlFile, "moreYield.yield#Multiplier",moreYield.settings.Multiplier)
+	
+	saveXMLFile(xmlFile)
+	delete(xmlFile)
+end
+
+function moreYield:loadSettings()
+	local xmlPath = getUserProfileAppPath() .. "modSettings" .. "/" .. "moreYield.xml"
+	
+	if fileExists(xmlPath) then
+		local xmlFile = loadXMLFile("moreYield", xmlPath)
+		
+		if xmlFile == 0 then
+			moreYield:defSettings()
+			return
+		end
+
+		local Multiplier = Utils.getNoNil( getXMLFloat(xmlFile, "moreYield.yield#Multiplier"), 2);
+
+		if Multiplier < 1.5 then
+			Multiplier = 2
+		end
+		
+		if Multiplier > 100 then
+			Multiplier = 2
+		end
+		
+		moreYield.settings.Multiplier = Multiplier
+		moreYield.settings.OldMultiplier = Multiplier
+		
+		delete(xmlFile)
+	else
+		moreYield:defSettings()
+	end
+end
+
+function moreYield:initUi()
+	if not moreYield.initUI then
+		local uiSettingsmoreYield = moreYieldUI.new(moreYield.settings)
+		
+		uiSettingsmoreYield:registerSettings()
+		
+		moreYield.initUI = true
+	end
+end
+
+addModEventListener(moreYield)
